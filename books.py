b71b736e5cac3e186194a0e9758ec522103155a4
EllaQuimica <ellaquimica@gmail.com>
2024-08-07 08:23:57 +0200

    POST Method

--- books.py
+++ books.py
@@ -1,5 +1,4 @@
-from fastapi import FastAPI
-
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -10,41 +9,29 @@ BOOKS = [
     {'title': 'Title Three', 'author': 'Author Three', 'category': 'history'},
     {'title': 'Title Four', 'author': 'Author Four', 'category': 'math'},
     {'title': 'Title Five', 'author': 'Author Five', 'category': 'math'},
-    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'},
-
+    {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
-@app.get("/")
-def first_api():
-    return {"message": "Hello SheCodifies!"}
 
-
-# Obtenemos todos los libros
-@app.get("/api-endpoint") #ruta estatica
-def first_api():
+@app.get("/books") #ruta estatica
+async def read_all_books(): 
     return BOOKS
-
-@app.get("/books/mybook") #ruta estatica
-def read_all_book():
-    return {'book_title': 'My favorite book!'}
-
-# Parametros dinamicos
-@app.get("/books/{dynamic_param}") #ruta dinamica
-def read_all_book(dynamic_param: str):
-    return {'dynamic_param': dynamic_param}
 
 #solicitud por titulo
 @app.get("/books/{book_title}")
-def read_book(book_title: str):
+async def read_book(book_title: str):
     for book in BOOKS:
         if book.get('title').casefold() == book_title.casefold():
             return book
         
+# Parametros dinamicos
+@app.get("/books/{dynamic_param}") #ruta dinamica
+def read_all_dynamic_book(dynamic_param: str):
+    return {'dynamic_param': dynamic_param}
 
 #Query Parameters -> esto es cuando el request URL:127.0.0.1:8000/books/?category=science
-
-@app.get("/books")
-def read_category_by_query(category: str):
+@app.get("/books/")
+async def read_category_by_query(category: str):
     books_to_return = []
     for book in BOOKS:
         if book.get('category').casefold() == category.casefold():
@@ -52,11 +39,43 @@ def read_category_by_query(category: str):
     return books_to_return
 
 
+# Get all books from a specific author using path or query parameters
+@app.get("/books/byauthor/")
+async def read_books_by_author_path(author: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == author.casefold():
+            books_to_return.append(book)
+
+    return books_to_return
+
 #Query Parameters -> esto es cuando el request URL:127.0.0.1:8000/books/author%20four/?category=science
-@app.get("/books/{boook_author}/")
+@app.get("/books/{book_author}/")
 async def read_author_category_by_query(book_author: str, category: str):
     books_to_return = []
     for book in BOOKS:
-        if book.get('author').casefold() == book_author.casefold() and book.get('category').casefold() == category.casefold():
+        if book.get('author').casefold() == book_author.casefold() and \
+                book.get('category').casefold() == category.casefold():
             books_to_return.append(book)
+
     return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
